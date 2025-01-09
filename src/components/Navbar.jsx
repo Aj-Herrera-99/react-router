a8488f4ea6d06c9e924665efc18c84647330f790
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-01-09 14:24:27 +0100

    gestione classe active su ogni NavLink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,29 @@ import { NavLink } from "react-router-dom";
 function Navbar() {
     return (
         <nav className="bg-blue-400 border-r-2 border-r-white">
-            <div className="flex flex-col gap-6 [&>a]:p-2 [&>a]:pr-8 [&>a]:border-b-2 [&>a]:text-white [&>a]:text-xl [&>a]:font-light [&>a]:tracking-widest [&>a]:uppercase hover:[&>a]:pb-4 [&>a]:transition-all [&>a]:rounded-md hover:[&>a]:bg-slate-700">
-                <NavLink to="/" className="">
+            <div className="flex flex-col gap-6 [&>a]:p-2 [&>a]:pr-8 [&>a]:border-b-2 [&>a]:text-white [&>a]:text-xl [&>a]:font-light [&>a]:tracking-widest [&>a]:uppercase hover:[&>a]:pb-4 [&>a]:transition-all [&>a]:rounded-md hover:[&>a]:bg-slate-800">
+                <NavLink
+                    to="/"
+                    className={({ isActive }) =>
+                        isActive ? " bg-slate-700" : ""
+                    }
+                >
                     Home
                 </NavLink>
-                <NavLink to="/pokedex" className="">
+                <NavLink
+                    to="/pokedex"
+                    className={({ isActive }) =>
+                        isActive ? " bg-slate-700" : ""
+                    }
+                >
                     Pokedex
                 </NavLink>
-                <NavLink to="/about" className="">
+                <NavLink
+                    to="/about"
+                    className={({ isActive }) =>
+                        isActive ? " bg-slate-700" : ""
+                    }
+                >
                     Chi Siamo
                 </NavLink>
             </div>
