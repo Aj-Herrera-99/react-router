13181c64d405c04031212a9893b99599e5e6429c
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-01-11 20:00:05 +0100

    responsiveness: hamburger menu su navbar completato

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const appLinks = [
@@ -11,30 +11,45 @@ const appLinks = [
 function Navbar() {
     const [isPokedexOpen, setIsPokedexOpen] = useState(false);
     const [view, setView] = useState(false);
-    const navElClass =
-        "p-2 border-b-2 text-white text-xl font-light tracking-widest uppercase hover:pb-4 transition-all rounded-md hover:bg-slate-800 cursor-pointer";
+    const navRef = useRef(null);
 
     const mobileView = window.matchMedia("(max-width: 640px)");
     mobileView.addEventListener("change", function () {
         this.matches ? setView(false) : setView(true);
     });
 
+    const handleHamClick = (e) => {
+        if (navRef.current) {
+            navRef.current.classList.toggle("!block");
+            navRef.current.classList.toggle("!translate-x-0");
+            e.currentTarget.classList.toggle("translate-x-[140px]");
+            e.currentTarget.classList.toggle("fa-xmark");
+        }
+    };
+
     useEffect(() => {
         window.innerWidth < 640 ? setView(false) : setView(true);
     }, []);
+
+    // classes
+    const navElClass =
+        "p-2 border-b-2 text-white text-xl font-light tracking-widest uppercase hover:pb-4 transition-all rounded-md hover:bg-slate-800 cursor-pointer";
+
     return (
         <>
             <i
-                class={`${
+                onClick={handleHamClick}
+                className={`${
                     view && "hidden"
-                } fa-solid fa-bars absolute top-0 left-0 text-white z-20 text-3xl cursor-pointer`}
+                } fa-solid fa-bars absolute top-0 left-0 text-white z-20 text-3xl cursor-pointer opacity-50 hover:opacity-100 transition-all`}
             ></i>
             <nav
+                ref={navRef}
                 className={`${
-                    !view && "absolute hidden"
-                } z-10 top-0 left-0 bottom-0  border-r-2 bg-slate-950 border-r-white min-w-[200px]`}
+                    !view && "absolute translate-x-[-200px]"
+                } z-10 top-0 left-0 bottom-0 border-r-2 bg-slate-950 border-r-white min-w-[200px] transition-all`}
             >
-                <div className="flex flex-col gap-6">
+                <div className="flex flex-col gap-6 mt-10 sm:mt-0">
                     {appLinks
                         .filter((link) => !link.to.includes("pokedex"))
                         .map((link, index) => (
